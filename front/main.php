9cadea61ea75e3e5508c00897a8726813bc5a522
youyu <129youyu@gmail.com>
2024-12-17 09:40:12 +0800

    在首頁顯示最新消息資料及more文字_main加入if判斷五筆資料_ul中用foreach抓文字內容及彈出視窗

--- front/main.php
+++ front/main.php
@@ -56,19 +56,46 @@
     <div
         style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
         <span class="t botli">最新消息區
+            <?php
+            if($News->count(['sh'=>1])>5){
+                echo "<a href='index.php?do=news' style='float:right'>";
+                echo "More...";
+                echo "</a>";
+            }
+            ?>
         </span>
+        <!-- 要顯示符合條件的前五筆 -->
         <ul class="ssaa" style="list-style-type:decimal;">
+            <?php
+            $all_news=$News->all(['sh'=>1]," limit 5");
+            foreach($all_news as $n){
+                echo "<li>";
+                echo mb_substr($n['text'],0,20);
+                echo "<span class='all' style='display:none'>";
+                echo $n['text'];
+                echo "</span>";
+                echo "</li>";
+            }
+            ?>
         </ul>
+        <!-- 彈出視窗 平常不顯示 滑鼠移上來才會顯示 -->
         <div id="altt"
             style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
-        </div>
+            123456</div>
         <script>
+        // ssaa在li的上層 針對ssaa下的li做的事情
+        // 滑鼠移上去才顯示            
         $(".ssaa li").hover(
             function() {
+                // 在altt裡面
+                // this => 滑鼠移上去的那一個 正在動作的那一個
+                // jQ的.html=原生js的innerHtml
+                // html() => 取得裡面的內容
                 $("#altt").html("<pre>" + $(this).children(".all").html() + "</pre>")
                 $("#altt").show()
             }
         )
+        // 滑鼠移開恢復不顯示
         $(".ssaa li").mouseout(
             function() {
                 $("#altt").hide()
